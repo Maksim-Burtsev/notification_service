ab3bcd02be93a44fcb6c285cfd2a2ee74f5848f9
MaximBurtsev <zadrot-lol@list.ru>
2022-07-29 09:11:03 +0300

    add celery & redis

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -115,3 +115,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
